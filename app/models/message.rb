e27b88e5ae78becb5b3b175f5ee7df480cd82245
bz0108rock-on <57309243+bz0108rock-on@users.noreply.github.com>
2019-12-04 21:27:09 +0900

    メッセージ送信機能実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validetes :content, presence: true, unless: :image?
+  validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
