9af600b4d70a6593c77b7d76f804006940a96812
bz0108rock-on <57309243+bz0108rock-on@users.noreply.github.com>
2019-12-20 19:20:22 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,12 @@
 $(function(){
+  
   function buildHTML(message){
     image = (message.image) ? `<img class="message-text__image" src=${message.image}>` : "";
 
-    var html = `<div class='message'>
+    var html = `<div class='message' data-message-id="${message.id}">
                   <div class='message__upper-info'>
                     <div class='message__upper-info__talker'>
-                      ${message.name}
+                      ${message.user_name}
                     </div>
                     <div class='message__upper-info__date'>
                       ${message.created_at}
@@ -24,7 +25,7 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -34,15 +35,38 @@ $(function(){
       contentType: false
     })
 
-    .done(function(data){
-      var html = buildHTML(data);
+    .done(function(message){
+      var html = buildHTML(message);
       $('.messages').append(html)
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('#new_message')[0].reset()
       $('input').prop('disabled', false);
     })
     .fail(function(){
-      alert('メッセージ送信に失敗しました')
+      alert('メッセージ送信に失敗しました');
+    });
+  });
+
+  var reloadMessages = function(){
+    last_message_id = $('.message:last').attr('data-message-id');
+    $.ajax({
+      url: 'api/messages#index {:format=>"json"}',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-  })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(messages) {
+      alert('自動更新に失敗しました');
+      console.log('error');
+    })
+  }
+  setInterval(reloadMessages, 3000);
 });
